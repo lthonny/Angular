8b05e64cb0706ce70e3a86ab367a616a36519cf5
thonny <thonny02@mail.ru>
2021-08-11 13:40:02 +0300

    add style project

--- @angular-app-blog/src/app/app.component.spec.ts
+++ @angular-app-blog/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-app-blog'`, () => {
+  it(`should have as title 'angular-blog'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-app-blog');
+    expect(app.title).toEqual('angular-blog');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-app-blog app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-blog app is running!');
   });
 });
