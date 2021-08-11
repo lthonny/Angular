0ba1b039d13d9f0db849cc90f66ba25c176c05d8
thonny <thonny02@mail.ru>
2021-08-11 19:09:21 +0300

    created folder architecture

--- @angular-app-blog/src/app/app-routing.module.ts
+++ @angular-app-blog/src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { PostPageComponent } from './post-page/post-page.component';
+import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: MainLayoutComponent, children: [
+      { path: '', redirectTo: '/', pathMatch: 'full' },
+      { path: '', component: HomePageComponent },
+      { path: 'post/:id', component: PostPageComponent }
+    ]
+  },
+  {
+    path: 'admin',
+    // loadChildren: './admin/admin.module#AdminModule'
+    loadChildren: () => import('./admin/admin.module').then(mod => mod.AdminModule),
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: PreloadAllModules
+  })],
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
